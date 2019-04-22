daef85a812701a1c42a2404e630c6ddf67fb4e79
prijatelj <theomnipotentwoof@gmail.com>
2019-04-22 18:36:32 -0400

    added looping to run sequential experiments with final.

--- exp_final.sh
+++ exp_final.sh
@@ -5,4 +5,6 @@ N2="../yeast+5rw_0.csv"
 GRN="../krogan_ground_truth.txt"
 
 # Note, spaces in the double quotes here breaks it. thanks matlab.
-matlab -nodesktop -nosplash -nodisplay -nojvm -r "known_mapping_test('$N1','$N2','$GRN');exit;" 
+for filename in ../yeast_rw/*.csv; do
+    matlab -nodesktop -nosplash -nodisplay -nojvm -r "known_mapping_test('$N1','$filename','$GRN');exit;" 
+done
