f4324b48c4d85e4769180c0504a2d90af8ad00e1
prijatelj <theomnipotentwoof@gmail.com>
2019-04-22 14:35:09 -0400

    added conversion of table to arrays (matrix)

--- algs_project_testing_known_mapping.m
+++ algs_project_testing_known_mapping.m
@@ -1,8 +1,8 @@
-krogan = readtable('../krogan_2007_adj_mat.csv');
+krogan = table2array(readtable('../krogan_2007_adj_mat.csv'));
 krogan_node_label =[];
 krogan_edge_label = {};
 
-noisy = readtable('../yeast+5rw_0.csv');
+noisy = table2array(readtable('../yeast+5rw_0.csv'));
 noisy_node_label = []; 
 noisy_edge_label = {}; 
 
@@ -14,7 +14,7 @@ alpha = 0.3; maxiter = 30; tol = 1e-4;
 S = FINAL(krogan, noisy, krogan_node_label, noisy_node_label, krogan_edge_label, noisy_edge_label, H, alpha, maxiter, tol);
 
 %% Saves the matrix:
-csvwrite('krogan_vs_yeast+5rw_0.csv', S)
+csvwrite('krogan_vs_yeast+5rw_0.csv', S);
 
 %% commented out due to missing ground truth atm, or idk what it is.
 %% M = greedy_match(S);
