2557fa4130736374e27771988008cddb938a0951
prijatelj <theomnipotentwoof@gmail.com>
2019-04-22 15:36:06 -0400

    fixing the output file name.

--- known_mapping_test_script.m
+++ known_mapping_test_script.m
@@ -16,8 +16,15 @@ alpha = 0.3; maxiter = 30; tol = 1e-4;
 S = FINAL(net1, noisy, net1_node_label, noisy_node_label, net1_edge_label, noisy_edge_label, H, alpha, maxiter, tol);
 
 %% Saves the matrix:
-csvwrite(strcat(substring(netpath1, 1..strlength(netpath1)-4), '_vs_', substring(netpath1, 1..strlength(netpath1)-4)), S);
+netpath1_substr = strsplit(netpath1, '.')
+netpath1_substr = netpath1_substr{size(netpath1_substr,2)-1}
 
+netpath2_substr = substring(netpath1, 1..(strlength(netpath1)-4))
+netpath2_substr = netpath2_substr{size(netpath2_substr,2)-1}
+
+csvwrite(strcat(netpath1_substr, '_vs_', netpath2_substr, '.csv'), S);
+
+%% Check ground truth accuracy
 ground_truth = table2array(readtable(ground_truth_path));
 M = greedy_match(S);
 [row, col] = find(M);
