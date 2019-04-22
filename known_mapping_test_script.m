7f047041688b88ab4d5fb03a4964fb92eebfea86
prijatelj <theomnipotentwoof@gmail.com>
2019-04-22 15:13:18 -0400

    WIP: function for running final on two networks with a given ground truth

---
+++ known_mapping_test_script.m
@@ -0,0 +1,27 @@
+function final_pair(netpath1, netpath2, ground_truth_path)
+
+net1 = table2array(readtable(netpath1));
+net1_node_label =[];
+net1_edge_label = {};
+
+noisy = table2array(readtable(netpath2));
+noisy_node_label = []; 
+noisy_edge_label = {}; 
+
+%% Uniform similarity matrix.
+H = ones(size(noisy, 1), size(net1, 1)) / (size(noisy, 1) + size(net1, 1))
+
+%% run net1 vs noisy
+alpha = 0.3; maxiter = 30; tol = 1e-4;
+S = FINAL(net1, noisy, net1_node_label, noisy_node_label, net1_edge_label, noisy_edge_label, H, alpha, maxiter, tol);
+
+%% Saves the matrix:
+csvwrite(strcat(substring(netpath1, 1..strlength(netpath1)-4), '_vs_', substring(netpath1, 1..strlength(netpath1)-4)), S);
+
+ground_truth = table2array(readtable(ground_truth_path));
+M = greedy_match(S);
+[row, col] = find(M);
+acc = size(intersect([col row], ground_truth, 'rows'), 1)/size(ground_truth, 1)
+
+exit;
+end;
