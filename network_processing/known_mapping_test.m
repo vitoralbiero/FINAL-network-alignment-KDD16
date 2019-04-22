10782e0d63f3aa94b45789c6042bbcd1a6073526
prijatelj <theomnipotentwoof@gmail.com>
2019-04-22 16:22:45 -0400

    added semi colons to avoid print dump in terminal.

--- network_processing/known_mapping_test.m
+++ network_processing/known_mapping_test.m
@@ -9,18 +9,18 @@ noisy_node_label = [];
 noisy_edge_label = {}; 
 
 %% Uniform similarity matrix.
-H = ones(size(noisy, 1), size(net1, 1)) / (size(noisy, 1) + size(net1, 1))
+H = ones(size(noisy, 1), size(net1, 1)) / (size(noisy, 1) + size(net1, 1));
 
 %% run net1 vs noisy
 alpha = 0.3; maxiter = 30; tol = 1e-4;
 S = FINAL(net1, noisy, net1_node_label, noisy_node_label, net1_edge_label, noisy_edge_label, H, alpha, maxiter, tol);
 
 %% Saves the matrix:
-netpath1_substr = strsplit(netpath1, '.')
-netpath1_substr = netpath1_substr{size(netpath1_substr,2)-1}
+netpath1_substr = strsplit(netpath1, '.');
+netpath1_substr = netpath1_substr{size(netpath1_substr,2)-1};
 
-netpath2_substr = strsplit(netpath2, '.')
-netpath2_substr = netpath2_substr{size(netpath2_substr,2)-1}
+netpath2_substr = strsplit(netpath2, '.');
+netpath2_substr = netpath2_substr{size(netpath2_substr,2)-1};
 
 csvwrite(strcat(netpath1_substr, '_vs_', netpath2_substr, '.csv'), S);
 
