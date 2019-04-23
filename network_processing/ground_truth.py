ec0cd8b0c6721926c755ecd8e791c12de78964a4
Vitor Albiero <vitor_albiero@outlook.com>
2019-04-23 16:55:31 -0400

    Created ground truth using random node indices and save the matrix with random node indices

--- network_processing/ground_truth.py
+++ network_processing/ground_truth.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Convert the LEDA graph representation into an adj matrix.')
+    parser = argparse.ArgumentParser(description='Generate ground truth for the network.')
     parser.add_argument('input_file', default=None, help='Enter the file path to the input file.')
     parser.add_argument('-o', '--output_file', default=None, help='Enter the file path to output file.')
 
@@ -11,15 +11,23 @@ def parse_args():
 
 
 if __name__ == '__main__':
+    np.random.seed(42)
     args = parse_args()
 
-    edge_matrix = np.loadtxt(args.input_file, delimiter=' ', dtype=np.str)
+    edge_matrix = np.loadtxt(args.input_file, delimiter=',', dtype=np.int)
 
-    idx = np.linspace(1, len(edge_matrix), len(edge_matrix))
-    ground_truth = np.transpose(np.array([idx, idx]))
-    print(ground_truth.shape)
+    idx1 = np.linspace(0, len(edge_matrix) - 1, len(edge_matrix)).astype('int')
+    np.random.shuffle(idx1)
+
+    idx2 = np.linspace(0, len(edge_matrix) - 1, len(edge_matrix)).astype('int')
+
+    edge_matrix_flipped = edge_matrix
+    edge_matrix_flipped[idx1, :][:, idx1] = edge_matrix[idx2, :][:, idx2]
+
+    ground_truth = np.transpose(np.array([idx1 + 1, idx2 + 1]))
 
     if args.output_file is None:
         args.output_file = './ground_truth.csv'
 
-    np.savetxt(args.output_file, ground_truth, delimiter=' ', fmt='%i')
+    np.savetxt(args.output_file, ground_truth, delimiter=',', fmt='%i')
+    np.savetxt(args.input_file[:-4] + '_random.csv', edge_matrix_flipped, delimiter=',', fmt='%i')
